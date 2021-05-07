5628359ac15036f5e04a0050e8ce2be3c6938dbe
FrankShi9 <53264047+FrankShi9@users.noreply.github.com>
2021-05-07 10:45:44 +0800

    teacher's answer

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdlib.h>
 
 int a = 20, b = 100, result1 = 0, result2 = 0, result3 = 0;
 
@@ -9,36 +10,53 @@ struct Param{
     int* B;
 };
 
-void* plus(struct Param input){
-    result1 = *input.A + *input.B;
+void* plus(void* arg){
+    result1 = a + b;
+    // We will store the result in heap memory.
+    //We will allocate a block of memory in heap of a size equal to the size of an integer (4 Bytes).
     printf("First thread calculates a + b. The result 1 is: %d\n", result1);
-    return NULL;
+    int *pointer1 = (int *) malloc(sizeof(int));
+    *pointer1 = result1;
+    return pointer1;
 }
 
-void* times(struct Param input){
-    result2 = *input.A * *input.B;
+void* times(void* arg){
+    result2 = a * b;
     printf("Second thread calculates a * b. The result 2 is: %d\n", result2);
-    return NULL;
+    int *pointer2 = (int *) malloc(sizeof(int));
+    *pointer2 = result2;
+    return result2;
 }
 
-void* minus(struct Param input){
-    result3 =  *input.B - *input.A;
+void* minus(void* arg){
+    result3 =  b - a;
     printf("Third thread calculates a - b. The result 3 is: %d\n", result3);
-    return NULL;
+    int *pointer3 = (int *) malloc(sizeof(int));
+    *pointer3 = result3;
+    return pointer3;
 }
 
 
 
 int main(){
-    struct Param argument = {&a, &b};
+//    struct Param argument = {&a, &b};
     pthread_t tid[3];
-    pthread_create(&tid[0], NULL, (void *(*)(void *)) plus, (void*)(&argument));
-    pthread_create(&tid[1], NULL, (void *(*)(void *)) times,(void*)(&argument));
-    pthread_create(&tid[2], NULL, (void *(*)(void *)) minus,(void*)(&argument));
+
+    int *ptr1, *ptr2, *ptr3;
+
+    pthread_create(&tid[0], NULL, plus, NULL);
+    pthread_create(&tid[1], NULL, times,NULL);
+    pthread_create(&tid[2], NULL, minus,NULL);
     
-    pthread_join(tid[0],NULL);
-    pthread_join(tid[1],NULL);
-    pthread_join(tid[2],NULL);
+    pthread_join(tid[0],(void*)&ptr1);
+    pthread_join(tid[1],(void*)&ptr2);
+    pthread_join(tid[2],(void*)&ptr3);
+
+
+    printf("First thread calculates a + b. The result 1 is: %d\n", *ptr1);
+    printf("Second thread calculates a * b. The result 2 is: %d\n", *ptr2);
+    printf("Third thread calculates a - b. The result 3 is: %d\n", *ptr3);
+
     printf("Let's combine the results like this: result 1 + result 2 - result 3.\n");
     printf("The final value is: %d\n",result1+result2-result3);
     return 0;
